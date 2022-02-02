ed876a36c2f7cdde47cd4b1de2767762bb6846ce
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:17:06 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,9 +1,9 @@
 #lang racket
 
-; (reduce f init lst): F X V X L -> V
+; (list.reduce f init lst): F X V X L -> V
 ; reduces list of values to a single value
-(define reduce
+(define list.reduce
   (lambda (f init lst)
     (if (null? lst)
         init
-        (f (car lst) (reduce f init (cdr lst))))))
+        (f (car lst) (list.reduce f init (cdr lst))))))
